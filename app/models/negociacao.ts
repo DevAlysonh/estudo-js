3b3aae8d210313353cbadf824c3c4093c7c335fb
dev.alysonh <dev.alysonh@gmail.com>
2023-04-02 19:00:39 -0300

    Add ts config, and config compiler

---
+++ app/models/negociacao.ts
@@ -0,0 +1,31 @@
+export class Negociacao {
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+    get getData()
+    {
+        return this.#data;
+    }
+
+    get getQuantidade()
+    {
+        return this.#quantidade;
+    }
+
+    get getValor()
+    {
+        return this.#valor;
+    }
+
+    get getVolume()
+    {
+        return this.#quantidade * this.#valor;
+    }
+}
