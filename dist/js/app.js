2a7349711d8c80719017b2af11b56534d08b790c
dev.alysonh <dev.alysonh@gmail.com>
2023-04-02 18:27:32 -0300

    Create first model

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,7 @@
-alert('oi');
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao.getVolume);
+console.log(negociacao.getData);
+console.log(negociacao.getQuantidade);
+console.log(negociacao.getVolume);
