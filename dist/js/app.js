7fb6340854fc41bf5c0fdd9e2b34b7b5a078d462
dev.alysonh <dev.alysonh@gmail.com>
2023-04-03 21:04:39 -0300

    Change study

--- dist/js/app.js
+++
@@ -1,6 +0,0 @@
-import { Negociacao } from "./models/negociacao";
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.getVolume);
-console.log(negociacao.getData);
-console.log(negociacao.getQuantidade);
-console.log(negociacao.getVolume);
