d7691136c97de9907984aa937ac603bf53479d92
dev.alysonh <dev.alysonh@gmail.com>
2023-04-04 21:44:30 -0300

    Type object

--- app.ts
+++ app.ts
@@ -1,16 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
+const person = {
+    name: 'Alyson',
+    age: 29
+};
 
-    if(showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-}
-
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var phrase = 'The result is: ';
-
-add(number1, number2, printResult, phrase);
+console.log(person.name);
