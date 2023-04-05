37118ea351ae70235870b76d1d77a9a3c0074ba5
dev.alysonh <dev.alysonh@gmail.com>
2023-04-04 21:22:46 -0300

    Working with types

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result = n1 + n2;
+
+    if(showResult) {
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
 }
 
 var number1 = 5;
 var number2 = 2.8;
+var printResult = true;
+var phrase = 'The result is: ';
 
-const result = add(number1, number2);
-
-console.log(result);
+add(number1, number2, printResult, phrase);
