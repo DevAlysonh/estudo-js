6b0b34b690379f9e02d4bea3673dbb1e70e0d3ca
dev.alysonh <dev.alysonh@gmail.com>
2023-04-06 21:09:26 -0300

    Studying

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 const person = {
     name: 'Alyson',
-    age: 29
+    age: 29,
+    hobbies: ['Pay Guitar', 'Study', 'Party']
 };
 
-console.log(person.name);
+console.log(`${person.name} likes: `);
+
+for (const hobbie of person.hobbies) {
+    console.log(hobbie.toUpperCase());
+}
