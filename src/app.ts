cc408f8ae1c98cf090bc5f4436e94c9e41681ad4
dev.alysonh <dev.alysonh@gmail.com>
2023-04-17 19:53:13 -0300

    Study

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,13 @@ function add(a: number, b: number) {
     return result;
 }
 
-console.log(add(10, 20));
+const addArrow = (a: number, b: number) => a + b;
+const button = document.querySelector('button');
+
+if(button) {
+    let clicks: number = 0;
+    button.addEventListener('click', () => {
+        let quant = clicks++;
+        console.log(quant);
+    });
+}
